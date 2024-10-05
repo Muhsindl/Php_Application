7c3f7050f7cf55586797e80a788d9de16b99f344
Muhsin DOLU <muhsindolu06@gmail.com>
2024-10-05 23:04:07 +0300

    Sayı döngü uygulaması
    Kullanıcıya 0-10 arasında ki sayıları alt alta ekranda gösteren fonksiyon

---
+++ sayiDonguUygulamasi.php
@@ -0,0 +1,18 @@
+<?php
+# Arttırma ve azaltma
+$sayi=10;
+$sayi++;
+echo "++ kullanımı: ".$sayi;
+
+echo "<br>";
+
+$sayi=10;
+$sayi--;
+echo "-- kullanımı: ".$sayi;
+
+echo "<br>";
+
+for($i=0; $i<10; $i++){
+	print $i."<br>"; // echo'da yazabilirdik
+}
+?>
