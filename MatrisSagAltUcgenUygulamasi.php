cb63a0e09c475b6c1182dcc9ddcf7255b9ee55ef
Muhsin DOLU <94922888+Muhsindl@users.noreply.github.com>
2025-08-02 18:32:14 +0300

    Matris sağ alt üçgeni yıldız yapan uygulama
    Kullanıcıdan alınan matris boyutuna göre sağ alt üçgeni yıldız olarak ayarlayan geriye kalan elemanları ise kullanıcıdan tarafından girilen matris uygulaması

---
+++ MatrisSagAltUcgenUygulamasi.php
@@ -0,0 +1,31 @@
+<?php
+
+// Kullanıcıdan matris boyutunu alıyoruz.
+echo "Kare matris için eleman sayısı: ";
+$eleman = trim(fgets(STDIN)); 
+
+// Boş bir dizi oluşturuyoruz.
+$dizi = array(); 
+
+for ($i = 0; $i < $eleman; $i++) {
+    for ($j = 0; $j < $eleman; $j++) {
+        // Sağ alt üçgen yıldız oluyor.
+        if($eleman<=$i+$j){
+            $dizi[$i][$j] ="*";
+        }else{
+            // Diğer elemanlarını kullanıcıdan alıyoruz
+            echo "Lütfen ${i},${j}'nci elemanı giriniz: "; 
+            $dizi[$i][$j] = trim(fgets(STDIN));
+        }
+    }
+}
+
+// Matrisin tüm elemanlarını yazdırıyoruz.
+echo "Girilen kare matris: \n";
+for ($i = 0; $i < $eleman; $i++) {
+    for ($j = 0; $j < $eleman; $j++) {
+        echo $dizi[$i][$j] . "\t";
+    }
+    echo "\n";
+}
+?>
