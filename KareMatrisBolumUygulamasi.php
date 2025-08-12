286d008066305d43f77220140393d1fa535e92c9
Muhsin DOLU <94922888+Muhsindl@users.noreply.github.com>
2025-08-12 08:01:33 +0300

    Kare matris bölme uygulaması
    Kullanıcı tarafından girilen elaman sayısı ve 2 adet kare matris elemanlarını bölüp ekranda yeni matris ile yazdıran uygulama

---
+++ KareMatrisBolumUygulamasi.php
@@ -0,0 +1,49 @@
+<?php
+
+// Kullanıcıdan kare matrisin boyutunu alıyoruz
+echo "Kare matris için eleman sayısını giriniz: ";
+$eleman = trim(fgets(STDIN)); 
+
+// Matrisleri saklamak için üç boş dizi oluşturuyoruz
+$dizi1 = array(); 
+$dizi2 = array(); 
+$dizi3 = array();
+
+// İlk matrisin elemanlarını kullanıcıdan alıyoruz.
+echo "Birinci matrisin elemanlarını giriniz:\n";
+for ($i = 0; $i < $eleman; $i++) {
+    for ($j = 0; $j < $eleman; $j++) {
+        // Kullanıcıdan her bir elemanı alıyoruz.
+        echo "${i},${j} elemanını giriniz: "; 
+        $dizi1[$i][$j] = trim(fgets(STDIN));
+    }
+}
+
+// İkinci matrisin elemanlarını kullanıcıdan alıyoruz.
+echo "İkinci matrisin elemanlarını giriniz:\n";
+for ($i = 0; $i < $eleman; $i++) {
+    for ($j = 0; $j < $eleman; $j++) {
+        // Kullanıcıdan her bir elemanı alıyoruz.
+        echo "${i},${j} elemanını giriniz: "; 
+        $dizi2[$i][$j] = trim(fgets(STDIN));
+    }
+}
+
+// Matrislerin bölümünü ( Matris1/ Matris2) hesaplıyoruz ve üçüncü matrise kaydediyoruz.
+for ($i = 0; $i < $eleman; $i++) {
+    for ($j = 0; $j < $eleman; $j++) {
+        // İki matrisin karşılık gelen elemanlarını bölüyoruz.
+        $dizi3[$i][$j] = $dizi1[$i][$j] / $dizi2[$i][$j];
+    }
+}
+
+// Matrisin bölümünü ekrana yazdırıyoruz.
+echo "Girilen matrislerin bölüm (Matris1 / Matris2) sonucu oluşan matris: \n";
+for ($i = 0; $i < $eleman; $i++) {
+    for ($j = 0; $j < $eleman; $j++) {
+        echo $dizi3[$i][$j] . " "; // Her bir elemanı ekrana yazdırıyoruz.
+    }
+    echo "\n";
+}
+
+?>
